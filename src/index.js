97eb89ce37f09c1e6494bf9b7b5aa7033825fb4a
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-11-13 19:46:47 +0100

    dispaly changing

--- src/index.js
+++ src/index.js
@@ -151,10 +151,13 @@ function onClick(event) {
   raycaster.setFromCamera(mouse, camera);
 
   var intersects = raycaster.intersectObjects(scene.children, true);
+  const dispalyDetails = document.getElementsByClassName('display')[0]
 
   if (intersects.length > 0) {
 
     console.log('Intersection:', intersects[0]);
+    const dispalyDetails = document.getElementsByClassName('display')[0];
+    dispalyDetails.style.visibility='visible'
 
   }
 
